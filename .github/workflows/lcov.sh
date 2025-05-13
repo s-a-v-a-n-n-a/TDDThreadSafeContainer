0f8a04df343613f858a4cce71580fc33144e683f
s-a-v-a-n-n-a <savchuk.ao@phystech.edu>
2025-05-13 22:12:02 +0000

    FIX: try to fix ci

--- .github/workflows/lcov.sh
+++ .github/workflows/lcov.sh
@@ -5,7 +5,7 @@ cd build
 ./tests/test_threadsafe_storage
 
 echo "gathering coverage..."
-lcov --capture --directory . --output-file coverage.info
+lcov --capture --directory $PWD --output-file coverage.info
 lcov --remove coverage.info '*/tests/googletest/*' '/usr/*' --output-file coverage.filtered.info
 lcov --list coverage.filtered.info
 genhtml -o cov-report/ coverage.filtered.info
