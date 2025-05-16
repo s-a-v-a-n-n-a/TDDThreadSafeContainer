50b82d8644c6cb9729f0e70fdf26712fb5c79302
s-a-v-a-n-n-a <savchuk.ao@phystech.edu>
2025-05-16 16:58:09 +0000

    FIX: lcov script

--- .github/workflows/lcov.sh
+++ .github/workflows/lcov.sh
@@ -4,8 +4,10 @@ echo "testing..."
 cd build
 ./test_threadsafe_storage
 
+LCOV_ARGS=(--rc lcov_branch_coverage=1)
+
 echo "gathering coverage..."
-lcov --directory .. --capture --output-file coverage.info --function-coverage --branch-coverage --rc geninfo_unexecuted_blocks=1 --ignore-errors mismatch,mismatch
-lcov --remove coverage.info '*/tests/*' '/usr/*' --output-file coverage.filtered.info
-lcov --summary coverage.filtered.info
-genhtml -o cov-report/ coverage.filtered.info --rc lcov_branch_coverage=1 --rc geninfo_no_exception_branch=1
+lcov --directory .. --capture --output-file coverage.info "${LCOV_ARGS[@]}"
+lcov --remove coverage.info '*/tests/*' '/usr/*' --output-file coverage.filtered.info "${LCOV_ARGS[@]}"
+lcov --summary coverage.filtered.info "${LCOV_ARGS[@]}"
+genhtml -o cov-report/ coverage.filtered.info "${LCOV_ARGS[@]}"
